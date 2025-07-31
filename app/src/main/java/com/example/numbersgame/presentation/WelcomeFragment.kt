da8cdbed9fc483c6d85708d7374295ece889864a
MaximOdincov <124523356+MaximOdincov@users.noreply.github.com>
2025-07-31 08:45:31 +0500

    create all fragments

---
+++ app/src/main/java/com/example/numbersgame/presentation/WelcomeFragment.kt
@@ -0,0 +1,24 @@
+package com.example.numbersgame.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.numbersgame.R
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+
+}
